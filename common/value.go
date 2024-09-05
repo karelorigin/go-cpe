e28716b9abdfd75e722093c4bf75fc9df6e93a1f
Karel <karel.origin@gmail.com>
2024-09-05 13:31:50 +0200

    Fix comments

--- common/value.go
+++ common/value.go
@@ -1,12 +1,12 @@
 package common
 
-// Value represents a
+// Value represents a value in raw and quoted form.
 type Value struct {
 	Quoted string
 	Raw    string
 }
 
-// String always return the quoted value representation.
+// String always returns the quoted value representation.
 func (v Value) String() string {
 	return v.Quoted
 }
